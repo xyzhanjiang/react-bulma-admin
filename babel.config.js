7c80c5c346fcb9c7cc02276ef0af7d889b1e48bd
xyzhanjiang <xyzhanjiang@qq.com>
2021-04-12 16:07:01 +0800

    fix: exclude transform regenerator

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   presets: [
-    '@babel/preset-env',
+    ['@babel/preset-env', {
+      exclude: ['@babel/plugin-transform-regenerator']
+    }],
     '@babel/preset-react'
   ],
   plugins: [[
